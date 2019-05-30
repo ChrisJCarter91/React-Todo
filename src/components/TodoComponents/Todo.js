fc2df10d04a7681fe86186af691f23451317a172
chris.j.carter <chris.j.carter91@gmail.com>
2019-05-30 08:59:00 -0600

    MVP

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Todo = props => {
+    return (
+      <div
+      /*ternary operator to toggle a line through the children*/
+        style={props.todo.completed ? { textDecoration: 'line-through' } : null}
+        onClick={() => props.handleToggleComplete(props.todo.id)}
+      >
+        {props.todo.task}
+      </div>
+    );
+  };
+  
+  export default Todo;
